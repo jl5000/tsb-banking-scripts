1304acf2135d627f3a8a56a357bda583f442a5ed
jl5000 <jalendrum@gmail.com>
2018-01-14 23:26:10 +0000

    tidy up main script

--- main.R
+++ main.R
@@ -12,14 +12,14 @@ df <- df %>% remove_transaction(9000, "01/08/2017", "31/08/2017")
 df <- df %>% remove_transaction(1000, "05/01/2018", "09/01/2018")
 
 
-#check misc entries
+#check misc entries to see if any additions need to be made to personal.R
 df %>%
   filter(t_class == "Misc") %>%
   filter(amount < 0) %>%
   distinct(t_desc) %>%
   View()
 
-#balance over time
+#core plot - balance over time
   df %>% 
     ggplot(aes(x=t_date,y=balance)) + geom_line() + geom_smooth(se=FALSE)
 
@@ -28,8 +28,7 @@ df %>%
     filter(amount < 0) %>%
     filter(t_class != "Special" & t_class != "Holidays") %>%
     ggplot(aes(x=t_month,y=abs(amount),fill=t_class)) + 
-      geom_bar(stat="identity") + 
-      geom_hline(yintercept = 2626)
+      geom_bar(stat="identity")
   
   #breakdown each month - class facets
   df %>%
@@ -49,7 +48,7 @@ df %>%
   
   #eating out spend per month
   df %>%
-    filter(t_class == "Shopping") %>%
+    filter(t_class == "Restaurants") %>%
     group_by(t_month) %>%
     summarise(total = abs(sum(amount))) %>%
     ggplot(aes(x=t_month, y=total)) +
